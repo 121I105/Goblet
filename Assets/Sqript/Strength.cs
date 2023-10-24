8e68f8192474752887c8aec6d2845980c5d2c13d
takumi020619 <140716694+takumi020619@users.noreply.github.com>
2023-10-25 02:14:56 +0900

    再アクティブ化
    非アクティブになった駒を再度アクティブに戻すために必要なコードは正しく実装されているように思われる。ただし、非アクティブな駒を再度アクティブにする際に何らかの問題が発生している可能性がある。

--- Assets/Sqript/Strength.cs
+++ Assets/Sqript/Strength.cs
@@ -13,6 +13,9 @@ public class Strength : MonoBehaviour
 {
     public PieceType type;
 
+    private bool isActive = true;
+    private Vector3 deactivatePosition; // 非アクティブ化された位置
+
     public int GetStrength()
     {
         switch (type)
@@ -30,7 +33,8 @@ public class Strength : MonoBehaviour
 
     void Start()
     {
-
+        // 初期位置を記憶
+        deactivatePosition = new Vector3(1000f, 1000f, 1000f); // デフォルトの非アクティブ位置
     }
 
     void Update()
@@ -40,7 +44,25 @@ public class Strength : MonoBehaviour
 
     public void ActivatePiece()
     {
+        isActive = true;
+        // デフォルトの非アクティブ位置に移動して再度表示
+        transform.position = deactivatePosition;
         gameObject.SetActive(true);
+    }
+
+    public void DeactivatePiece()
+    {
+        isActive = false;
+        // 現在の位置を非アクティブ位置として記憶
+        deactivatePosition = transform.position;
+        // 遠くの位置に移動して非アクティブ化
+        transform.position = new Vector3(1000f, 1000f, 1000f);
+        gameObject.SetActive(false);
+    }
+
+    public bool IsPieceActive
+    {
+        get { return isActive; }
     }
 
     void OnCollisionEnter(Collision collision)
@@ -54,8 +76,8 @@ public class Strength : MonoBehaviour
 
             if (myStrength < otherPieceStrength)
             {
-                // 自分の駒の強さが小さい場合、自分の駒を非アクティブ化する
-                gameObject.SetActive(false);
+                // 自分の駒の強さが小さい場合、駒を非アクティブ化
+                DeactivatePiece();
                 Debug.Log("My piece is weaker. Disabling my piece.");
             }
         }
