cbe5d282e1f788ed64bc011ae225b23ee784c340
121I105 <121I105@stu.nit.ac.jp>
2023-10-26 09:22:19 +0900

    Revert "Revert "10/23""
    This reverts commit 3941accd4ddf60dced4714fe1e45a2e3d1e40f09.

--- Assets/Sqript/Piece.cs
+++ Assets/Sqript/Piece.cs
@@ -117,17 +117,6 @@ public class Piece : MonoBehaviour
                     }
                     // 最終的に駒の位置を更新
                     selectedPiece.position = newPiecePosition;
-                 
-
-                    // ここで勝利条件をチェック
-                    if (gameManager.CurrentPlayer == (int)PieceTeam.Blue)
-                    {
-                        gameManager.CheckStone("Player1");
-                    }
-                    else
-                    {
-                        gameManager.CheckStone("Player2");
-                    }
 
                     // ターンを切り替える
                     gameManager.SwitchTurn();
